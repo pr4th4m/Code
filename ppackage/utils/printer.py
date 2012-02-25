3b159ef3735e3bec0b55c96376b7506c6b08f6c1
pratz <pratz0909@gmail.com>
2012-02-25 20:00:43 +0530

    added ppackage

--- ppackage/utils/printer.py
+++ ppackage/utils/printer.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
+# Module providing utilities for printing stuff
 import pprint
 
 def pretty(data_struct) :
     """ just a wrapper round the standard python module pprint
+    Args : Data Structure
+    Returns : pprint Data Structure
     """
     pr = pprint.PrettyPrinter(indent=4)
     return pr.pprint(data_struct)
-
