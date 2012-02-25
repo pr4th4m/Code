46ef17d5a628527cd6187e66d42e7505143e75a2
pratz <pratz0909@gmail.com>
2012-02-26 00:55:46 +0530

    added strip_space

---
+++ ppackage/utils/strutils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Module providing utilities for string formatting and handling
+
+
+def strip_space(st):
+    """ Function to remove white space from beginning and end of string
+    Args : String
+    Returns : Stripped String
+    """
+    if st :
+        st = st.strip()
+
+    return st
